3a8bcd856912f93f6151ed6cedab236f5b8fc917
Jose Mauricio <Jose Mauricio@JoseMauricio>
2023-04-15 15:43:38 -0600

    Imports y javadoc

--- src/main/java/ipn/escom/ballScore/action/GestionarEquiposAction.java
+++ src/main/java/ipn/escom/ballScore/action/GestionarEquiposAction.java
@@ -1,6 +1,9 @@
 package ipn.escom.ballScore.action;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 import org.apache.commons.beanutils.BeanUtils;
 import org.apache.logging.log4j.LogManager;
